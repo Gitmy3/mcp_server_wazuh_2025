794a536fe84bf980f61bd57205811d069b27bbb8
Preetam Mukherjee <21f1002436@ds.study.iitm.ac.in>
2025-10-21 21:12:58 +0530

    Update config.py
    config file updated again.

--- app/config.py
+++ app/config.py
@@ -13,13 +13,14 @@ class Settings(BaseSettings):
     WAZUH_VERIFY_SSL: bool = False
 
     # 🧠 Add OpenAI key
-    OPENAI_API_KEY: str  = sk-proj-
-lsJQif0WlmHUtnKMyKFXuVHHxijxaSJZ9p2KW9RmBnWcGAOvOrKfCAcgmlfen1rlzdHFgRXrM6T3BlbkFJZFMYTvwEAvNXf7QsebACXLGK7KS_2Aqv4aPnwt9KvllKg4FJ1TkwyKLbj2JNtC_WZvgJhn8gUA
+    OPENAI_API_KEY: str  = "sk-proj-"
 
     class Config:
         env_file = ".env"
 
 settings = Settings()
+
+
 
 
 
