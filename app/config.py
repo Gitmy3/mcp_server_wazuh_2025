bb6201eb7b7a675454cb8d89b28cbdb4f47e45b8
Preetam Mukherjee <21f1002436@ds.study.iitm.ac.in>
2025-10-23 22:27:43 +0530

    Update config.py
    Config file updated with deleting the key

--- app/config.py
+++ app/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
     WAZUH_VERIFY_SSL: bool = False
 
     # 🧠 Add OpenAI key
-    OPENAI_API_KEY: str  = "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"
+    OPENAI_API_KEY: str  = "sk-proj-"
     
 
     class Config:
@@ -33,6 +33,9 @@ class Settings(BaseSettings):
         env_file = ".env"
 
 settings = Settings()
+
+
+
 
 
 
